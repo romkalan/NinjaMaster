8b821c9acac8f75531f5530ac6bded6b73bd841f
romkalan <romkalan@mail.ru>
2023-08-18 12:28:59 +0300

    add projectile and movement along a vector

--- NinjaMaster/Scenes/GameScene.swift
+++ NinjaMaster/Scenes/GameScene.swift
@@ -8,6 +8,38 @@
 import SpriteKit
 import GameplayKit
 
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+    CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+    CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+    CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+func sqrt(a: CGFloat) -> CGFloat {
+    CGFloat(sqrtf(Float(a)))
+}
+
+
+extension CGPoint {
+    func length() -> CGFloat {
+        sqrt(x * x + y * y)
+    }
+    
+    func normalized() -> CGPoint {
+        return self / length()
+    }
+}
+
+
 class GameScene: SKScene {
     
     var player: SKSpriteNode!
@@ -19,13 +51,30 @@ class GameScene: SKScene {
         player = PlayerSprite.populate(to: CGPoint(x: size.width * 0.1, y: size.height * 0.5))
         addChild(player)
         
-        run(
-            SKAction.repeatForever(
-                SKAction.sequence(
-                    [SKAction.run(addMonster), SKAction.wait(forDuration: 1.0)]
-                )
-            )
-        )
+        run(SKAction.repeatForever(SKAction.sequence(
+            [SKAction.run(addMonster), SKAction.wait(forDuration: 1.0)]
+        )))
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        
+        let projectile = SKSpriteNode(imageNamed: "projectile")
+        projectile.position = player.position
+        
+        let offset = touchLocation - projectile.position
+        if offset.x < 0 { return }
+        
+        addChild(projectile)
+                
+        let direction = offset.normalized()
+        let shootAmount = direction * 1000
+        let realDest = shootAmount + projectile.position
+        
+        let actionMove = SKAction.move(to: realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        projectile.run(SKAction.sequence([actionMove, actionMoveDone]))
     }
     
     func spawnMonsters() {
